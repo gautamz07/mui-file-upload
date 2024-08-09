f455b69ee9659a99bf2f282166455001f71ae483
Gautam <gautam.fonseca@matchmove.com>
2024-08-09 20:13:04 +0530

    [emergency commit] - pushing last state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { Button } from '@material-ui/core';
+import CloudUploadIcon from '@mui/icons-material/CloudUploadOutlined';
+// import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import axios from 'axios';
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+  input: {
+    display: 'none',
+  },
+}));
+
+export default function IconButtons() {
+  const classes = useStyles();
+  const [uploadProgress, setUploadProgress] = useState(0);
+
+  const handleFile = (fileData) => {
+    console.log('fileData')
+    console.log(fileData)
+    axios.post('/upload', fileData, {
+      onUploadProgress: (progressEvent) => {
+        const percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total);
+        setUploadProgress(percentCompleted);
+      }
+    })  
+  }
+
+  const handleChange = event => {
+    const fileUploaded = event.target.files[0];
+    handleFile(fileUploaded);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <input
+        accept="image/*"
+        className={classes.input}
+        id="contained-button-file"
+        multiple
+        type="file"
+        onChange={handleChange}
+      />
+      <label htmlFor="contained-button-file">
+        <Button variant="contained" color="primary" component="span" startIcon={<CloudUploadIcon />}>
+          Upload
+        </Button>
+      </label>
     </div>
   );
 }
-
-export default App;
